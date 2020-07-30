c536370c11b2f358b8f65d3cb99804923d918709
Yoshiyuki Ito <y.ito.0427@gmail.com>
2020-07-30 10:20:35 +0900

    Add files via upload

--- 3_Automate_sales_projections_with_Amazon_Forecast/lambdas/GetStatusForecastExportJob/getstatusforecastexportjob.py
+++ 3_Automate_sales_projections_with_Amazon_Forecast/lambdas/GetStatusForecastExportJob/getstatusforecastexportjob.py
@@ -3,7 +3,7 @@ def lambda_handler(event, context):
     forecast = boto3.client('forecast')
     
     response = forecast.describe_forecast_export_job(
-        ForecastExportJobArn='arn:aws:forecast:us-east-1:805433377179:forecast-export-job/uk_sales_add_20091201_20101216/uk_sales_add_20091201_20101216'
+        ForecastExportJobArn=event['ForecastExportJobArn']
     )
     
     if response['Status'] == 'ACTIVE':
