f03281a30bd2c323f085311b8cc8817cea014592
Yoshiyuki Ito <y.ito.0427@gmail.com>
2020-07-30 10:24:44 +0900

    Add files via upload

--- 3_Automate_sales_projections_with_Amazon_Forecast/lambdas/createforecastexportjob/forecastexportjob.py
+++ 3_Automate_sales_projections_with_Amazon_Forecast/lambdas/createforecastexportjob/forecastexportjob.py
@@ -2,14 +2,16 @@ def lambda_handler(event, context):
     import boto3
     forecast = boto3.client('forecast')
     
-    forecast.create_forecast_export_job(
-        ForecastExportJobName='uk_sales_add_20091201_20101216',
-        ForecastArn='arn:aws:forecast:us-east-1:805433377179:forecast/uk_sales_add_20091201_20101216',
+    response = forecast.create_forecast_export_job(
+        ForecastExportJobName=event['detail']['requestParameters']['key'].replace('input/','').replace('.csv',''),
+        ForecastArn=event['ForecastArn'],
         Destination={
             'S3Config': {
-                'Path': 's3://demo2-forecast-805433377179/output',
-                'RoleArn': 'arn:aws:iam::805433377179:role/service-role/AmazonForecast-ExecutionRole-1576658959279',
+                'Path': 's3://' + event['detail']['requestParameters']['bucketName'] + '/output',
+                'RoleArn': event['detail']['userIdentity']['sessionContext']['sessionIssuer']['arn'],
             }
         },
     )
+    
+    event['ForecastExportJobArn'] = response['ForecastExportJobArn']
     return event
